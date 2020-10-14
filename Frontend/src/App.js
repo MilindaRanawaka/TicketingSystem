69cc45fc176a0868896e7b8a8cb653aad32ce2fe
KavinduLiyanage <sandeepa8956@gmail.com>
2020-10-14 13:47:40 +0530

    Implemented Login function

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import { BrowserRouter, Switch } from "react-router-dom";
+import { Container } from "@material-ui/core";
 import './App.css';
+import PublicRoute from "./Components/ReactMiddleware/PublicRoute";
+import Login from "./Components/Login&Register/Login";
+import CustomerHomepage from "./Components/HomePage.Customer";
+import PrivateRoute from "./Components/ReactMiddleware/PrivateRoute";
 
 function App() {
   return (
@@ -10,17 +16,18 @@ function App() {
             <Switch>
 
               {/*All the Public Routes of System*/}
+              <PublicRoute restricted={true} component={Login} path="/" exact />
               <PublicRoute restricted={true} component={Login} path="/login" exact />
-              <PublicRoute restricted={true} component={CreateUser} path="/create-acc" exact />
+              {/*<PublicRoute restricted={true} component={CreateUser} path="/create-acc" exact />*/}
 
               {/*Admin(Public Transport Manager) Only Routes*/}
-              <PrivateRoute component={AdminHome} AccessBy={"admin"} path="/admin" exact />
+              {/*<PrivateRoute component={AdminHome} AccessBy={"admin"} path="/admin" exact />*/}
 
               {/*Ticket Inspector Only Routes*/}
-              <PrivateRoute component={CardViewProductListComponent} AccessBy={"storeManager"} path="/storeManager" exact />
+              {/*<PrivateRoute component={CardViewProductListComponent} AccessBy={"storeManager"} path="/storeManager" exact />*/}
 
               {/*Customer Only Routes*/}
-              <PrivateRoute component={WishList} AccessBy={"customer"} path="/wishList" exact />
+              <PrivateRoute component={CustomerHomepage} AccessBy={"customer"} path="/customerHome" exact />
 
             </Switch>
           </Container>
--- Frontend/src/Components/HomePage.Customer.js
+++ Frontend/src/Components/HomePage.Customer.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import ProductsList from "./Products/productsList";
-import { TOKEN_FNAME, TOKEN_LNAME } from "./config";
+import { TOKEN_UNAME } from "./config";
 
-export default class Homepage extends React.Component  {
+export default class CustomerHomepage extends React.Component  {
     render() {
         return (
             <div className="container">
                 <h1 align="center"> <span className="badge badge-dark">
-                    Welcome {localStorage.getItem(TOKEN_FNAME)} {localStorage.getItem(TOKEN_LNAME)}
+                    Welcome {localStorage.getItem(TOKEN_UNAME)}
                 </span></h1>
-                <ProductsList/>
+
             </div>
         );
     }
