a08cded994d1eb17f55c8ce02c9b926065fc0278
nirma03 <nirmasarangi@gmail.com>
2020-10-16 15:50:59 +0530

    retrieved data in a table for finances in admin portal

--- Frontend/src/Components/PerDay.Finances.js
+++ Frontend/src/Components/PerDay.Finances.js
@@ -1,71 +1,99 @@
 import React, { Component } from "react";
-import { MDBContainer,MDBMask,MDBView } from "mdbreact";
-import { BrowserRouter as Router } from 'react-router-dom';
 import {Card, CardBody, Col, Row, Table} from "reactstrap";
+import { MDBTable, MDBTableHead } from 'mdbreact';
 import FinanceNavBar from "./NavBar.Finances";
+import axios from 'axios';
+import {serverUrl} from "./config";
+
 
 class FinancePerDate extends Component {
-    // state = {
-    //     isOpen: false
-    // };
-    //
-    // toggleCollapse = () => {
-    //     this.setState({ isOpen: !this.state.isOpen });
-    // }
-    state = {
-        activeItem: "1"
+
+    constructor(props) {
+
+        super(props);
+        this.state = {
+
+            trips: [],
+            userDetails: [],
+
+        };
+    }
+
+    componentDidMount() {
+        axios
+            .get(serverUrl + "/trips/")
+            .then((response) => {
+                this.setState({
+                    trips: response.data,
+                });
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    getUsername(id){
+        axios
+            .get(serverUrl + "/users/" + id)
+            .then((response) => {
+                this.setState({
+                    userDetails: response.data,
+                });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     }
 
     render() {
         return (
-                        <div className="content">
+            <div className="content">
                 <FinanceNavBar/>
-            <Row>
-                <Col md="12">
-                    <Card>
-                        <CardBody>
-                            <Table responsive>
-                                <thead className="text-primary">
-                                <tr>
-                                    <th>Trip Date</th>
-                                    <th>User Id</th>
-                                    <th>Charge</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                <tr>
-                                    <td>Dakota Rice</td>
-                                    <td>Niger</td>
-                                    <td>Oud-Turnhout</td>
+                <Row>
+                    <Col md="12">
+                        <Card>
+                            <CardBody>
+                                <MDBTable hover>
+                                    <MDBTableHead className="text-primary">
+                                    {/*<thead className="text-primary">*/}
 
-                                </tr>
-                                <tr>
-                                    <td>Minerva Hooper</td>
-                                    <td>Curaçao</td>
-                                    <td>Sinaai-Waas</td>
-
-                                </tr>
-                                <tr>
-                                    <td>Sage Rodriguez</td>
-                                    <td>Netherlands</td>
-                                    <td>Baileux</td>
-                                </tr>
-                                <tr>
-                                    <td>Philip Chaney</td>
-                                    <td>Korea, South</td>
-                                    <td>Overland Park</td>
-                                </tr>
-                                </tbody>
-                            </Table>
-
-                        </CardBody>
-                    </Card>
-                </Col>
-            </Row>
-        </div>
+                                    <tr>
+                                        <th>Trip Date</th>
+                                        <th className="text-center">Passenger Name</th>
+                                        <th className="text-center">Trip Charge</th>
+                                    </tr >
+                                    </MDBTableHead>
+                                    <tbody>
+                                    {this.state.trips
+                                        .map((item) => {
+                                            return (
+                                                <tr key={item["_id"]}>
+                                                    {this.getUsername(item["userID"])}
+                                                    <td >{new Intl.DateTimeFormat("en-GB", {
+                                                        year: "numeric",
+                                                        month: "long",
+                                                        day: "2-digit",
+                                                        hour: 'numeric',
+                                                        minute: 'numeric'
+                                                    }).format(new Date(item["tripDateTime"]))}</td>
+                                                    <td className="text-center">{this.state.userDetails["username"]}</td>
+                                                    <td className="text-center">{item["charge"]}.00</td>
+                                                </tr>
+                                            );
+                                        })}
+                                    </tbody>
+                                </MDBTable>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
+            </div>
         );
     }
 }
 
 export default FinancePerDate;
+
+
+
 
