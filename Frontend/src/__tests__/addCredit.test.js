3191a72a79fd728ad102267ab1ea7ed3eb601b05
Ridmi95 <ridmi.creativehub@gmail.com>
2020-10-18 07:50:34 +0530

    done

--- Frontend/src/__tests__/addCredit.test.js
+++ Frontend/src/__tests__/addCredit.test.js
@@ -4,16 +4,23 @@ import AddCredit from '../Components/Passenger/AddCredit'
 
 //test cases for addCredit form
 describe('Register Component', () => {
+    //check the form if t is correct 
+    //pass test case
     it('should render without throwing an error', () => {
         expect(shallow(<AddCredit />).find('form.addCredit').exists()).toBe(true)
     })
-
+    //check the if cardHolderName length correct
+    //pass test case
     it('renders a username amout input text', () => {
         expect(shallow(<AddCredit />).find('#cardHolderName').length).toEqual(1)
     })
+    //check the if cardNumberId length correct
+    //pass test case
     it('renders a email fine amount text', () => {
         expect(shallow(<AddCredit />).find('#cardNumberId').length).toEqual(1)
     })
+    //check the incorrect test case
+    //fail test case
     it('renders a email fine amount text', () => {
         expect(shallow(<AddCredit />).find('#exampleInputEmail3').length).toEqual(1)
     })
